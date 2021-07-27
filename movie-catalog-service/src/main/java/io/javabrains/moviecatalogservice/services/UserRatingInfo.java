6357d5101f526daa6d6281b82d5115a5335fd5cc
Brandon Zhu <bzhu@utraque.com>
2021-07-27 00:38:04 -0400

    20. Configuring Hystrix parameters

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/services/UserRatingInfo.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/services/UserRatingInfo.java
@@ -1,6 +1,7 @@
 package io.javabrains.moviecatalogservice.services;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import io.javabrains.moviecatalogservice.models.Rating;
 import io.javabrains.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,13 @@ public class UserRatingInfo {
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+    @HystrixCommand(fallbackMethod = "getFallbackUserRating",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+            })
     public UserRating getUserRating(String userId) {
         return restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
     }
@@ -23,11 +30,8 @@ public class UserRatingInfo {
     private UserRating getFallbackUserRating(String userId) {
         UserRating userRating = new UserRating();
         userRating.setUserId(userId);
-        userRating.setRatings(Arrays.asList(
-                new Rating("0",0)
-        ));
+        userRating.setRatings(Arrays.asList(new Rating("0", 0)));
         return userRating;
     }
-
 
 }
